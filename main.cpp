239670f4609127fb68c84bb5411d53477f42ed31
Evan D <Evan_delasota@yahoo.com>
2021-03-10 10:01:37 +0000

    Added longest balanced span implementation

--- main.cpp
+++ main.cpp
@@ -7,14 +7,20 @@ std::vector<int> longestBalancedSpan(std::vector<int>);
 std::string telegraphConvert(std::string);
 
 int main() {
-  std::vector<int> vector { 3, 2, 5, 4, 5, 3, 2, 1, 0, 2, -3, -5, -3 };
+  std::vector<int> result;
+  std::vector<int> vector { 3, 2, 5, 4, 5, 3, 2, 1, 0, 2, -3, -7, -3 };
   std::cout << dipSearch(vector) << "\n";
-  std::cout << telegraphConvert("ab#c");
+
+  result = longestBalancedSpan(vector);
+  std::cout << result.at(0) << " " << result.at(1) << "\n";
+
+  std::cout << telegraphConvert("ab#      !?!?c!  ?2?   !") << "\n";
+  std::cout << telegraphConvert("ab#  ?ccb!  ?2?STOP");  
 }
 
 int dipSearch(std::vector<int> v) {
   int lastDip = v.at(0);
-  for (int i = 0; i < v.size() - 3; ++i) {
+  for (int i = 0; i < v.size() - 2; ++i) {
     if (v.at(i) == v.at(i + 2) && v.at(i) > v.at(i + 1)) {
       lastDip = i;
     }
@@ -23,25 +29,58 @@ int dipSearch(std::vector<int> v) {
   return lastDip;
 }
 
+std::vector<int> longestBalancedSpan(std::vector<int> v) {
+  std::vector<int> best;
+  int sum = 0;
+  int subArrayMin = 0;
+  int subArrayMax = 0;
+
+  for (int s = 0; s < v.size(); ++s) {
+    sum = v.at(s);
+    for (int e = s + 1; e < v.size(); ++e) {
+      sum += v.at(e);
+      if (sum == 0 && (e - s) > (subArrayMax - subArrayMin)) {
+        subArrayMax = e;
+        subArrayMin = s;
+      }
+    }
+  }
+  best.push_back(subArrayMin);
+  best.push_back(subArrayMax);
+  return best;
+}
 std::string telegraphConvert(std::string s) {
+  if (s.length() == 0) {
+    return s;
+  }
+
   std::string telegraph = "";
 
   for (int i = 0; i < s.length(); ++i) {
     char ch = s.at(i);
-    
-    if (ch == ' ' && s.at(i + 1) && (i + 1 < s.length())) {
-      continue;
+    bool firstSpace = true;
+
+    while (ch == ' ' && s.at(i + 1) == ' ' && (i + 1 < s.length())) {
+      if (firstSpace) {
+        telegraph.push_back(ch);
+        firstSpace = false;
+      }
+      i++;
     }
 
-    if (ch == )
-
-    if (ch - '0' >= 97 && ch - '0' <= 122) {
+    if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z')) {
+      telegraph.push_back(ch);
+    } else if (ch >= 'a' && ch <= 'z') {
       telegraph.push_back(toupper(ch));
     } else if (ch == '!' || ch == '?' || ch == ';') {
       telegraph.push_back('.');
     } else {
-      telegraph.push_back(ch);
+      continue;
     }
+  }
+
+  if (s.substr((s.length() - 4), s.length()) != "STOP") {
+    telegraph.append("STOP");    
   }
 
   return telegraph;
